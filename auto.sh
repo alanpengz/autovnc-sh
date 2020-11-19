aac5cca7af17e142c01dfcdaff992b7452361882
alanpengz <alanpengz@163.com>
2020-11-19 10:37:44 +0800

    a

--- auto.sh
+++ auto.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo cp vncserver /etc/init.d/ && sudo chmod 755 /etc/init.d/vncserver && sudo update-rc.d vncserver defaults && sudo reboot
+sudo /opt/vc/bin/tvservice -o && sudo cp vncserver /etc/init.d/ && sudo chmod 755 /etc/init.d/vncserver && sudo update-rc.d vncserver defaults && sudo reboot
